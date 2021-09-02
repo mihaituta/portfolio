1dc82c669f2d0f116f2245e0892a3437356b0c1d
tutamihai <tuta_mihai14@yahoo.com>
2021-09-02 23:25:24 +0200

    Reworked skills section, worked on media queries

--- map.js
+++ map.js
@@ -1,81 +1,56 @@
 function initGoogleMap() {
     let myLat = 51.364473520265456;
     let myLng = 6.419280449229745;
-    const latlng = new google.maps.LatLng(myLat, myLng);
-
-    /*   google.maps.Map.prototype.setCenterWithOffset = function (latlng, offsetX, offsetY) {
-           let map = this;
-           let ov = new google.maps.OverlayView();
-           ov.onAdd = function () {
-               let proj = this.getProjection();
-               let aPoint = proj.fromLatLngToContainerPixel(latlng);
-               aPoint.x = aPoint.x + offsetX;
-               aPoint.y = aPoint.y + offsetY;
-               map.setCenter(proj.fromContainerPixelToLatLng(aPoint));
-           }
-           ov.draw = function () {
-           };
-           ov.setMap(this);
-       };*/
+    const myCoords = new google.maps.LatLng(myLat, myLng);
 
     let map = new google.maps.Map(document.getElementById("map"), {
         zoom: 10,
-        center: latlng,
+        center: myCoords,
         mapId: '46d5929e81efda3',
         disableDefaultUI: true,
     })
 
     new google.maps.Marker({
-        position: latlng,
+        position: myCoords,
         map,
         title: "Mihai Tuta",
     });
 
-    function map_recenter(latlng, offsetx, offsety) {
-        var point1 = map.getProjection().fromLatLngToPoint(
+    // Offsets the map on x and y axes by given parameters
+    function map_recenter(latlng, offsetX, offsetY) {
+        let scale = Math.pow(2, map.getZoom());
+
+        let originPoint = map.getProjection().fromLatLngToPoint(
             (latlng instanceof google.maps.LatLng) ? latlng : map.getCenter()
         );
-        var point2 = new google.maps.Point(
-            ((typeof (offsetx) == 'number' ? offsetx : 0) / Math.pow(2, map.getZoom())) || 0,
-            ((typeof (offsety) == 'number' ? offsety : 0) / Math.pow(2, map.getZoom())) || 0
+        let offsetPoint = new google.maps.Point(
+            ((typeof (offsetX) == 'number' ? offsetX : 0) / scale) || 0,
+            ((typeof (offsetY) == 'number' ? offsetY : 0) / scale) || 0
         );
+
         map.setCenter(map.getProjection().fromPointToLatLng(new google.maps.Point(
-            point1.x - point2.x,
-            point1.y + point2.y
+            originPoint.x - offsetPoint.x,
+            originPoint.y + offsetPoint.y
         )));
     }
 
     google.maps.event.addListenerOnce(map, "projection_changed", function () {
-        if (window.matchMedia("(min-width: 769px)").matches) {
-
-            map_recenter(latlng, -330, 0);
+        if (window.matchMedia("(min-width: 1201px)").matches) {
+            map_recenter(myCoords, -350, 0);
+        } else if (window.matchMedia("(min-width: 769px)").matches) {
+            map_recenter(myCoords, -350, 0);
         } else {
-            map_recenter(latlng, -150, 0);
+            map_recenter(myCoords, -135, 0);
         }
 
         google.maps.event.addDomListener(window, 'resize', function () {
-            if (window.matchMedia("(min-width: 769px)").matches) {
-                map_recenter(latlng, -330, 0);
+            if (window.matchMedia("(min-width: 1201px)").matches) {
+                map_recenter(myCoords, -350, 0);
+            } else if (window.matchMedia("(min-width: 769px)").matches) {
+                map_recenter(myCoords, -350, 0);
             } else {
-                map_recenter(latlng, -150, 0);
+                map_recenter(myCoords, -135, 0);
             }
         })
     });
-
-
-    /* if (window.matchMedia("(min-width: 769px)").matches) {
-         map.setCenterWithOffset(latlng, 330, 0);
-     } else {
-         map.setCenterWithOffset(latlng, 150, 0);
-     }
-
-     google.maps.event.addDomListener(window, 'resize', function () {
-         if (window.matchMedia("(min-width: 769px)").matches) {
-             map.setCenterWithOffset(latlng, 330, 0);
-         } else {
-             map.setCenterWithOffset(latlng, 150, 0);
-         }
-
-     })*/
-
 }
