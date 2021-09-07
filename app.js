b45fdb0f71f17c2df59d543e4558e4ab8f277ec0
tutamihai <tuta_mihai14@yahoo.com>
2021-09-07 18:00:05 +0200

    Added validation for form's inputs

--- app.js
+++ app.js
@@ -7,9 +7,8 @@ const body = document.querySelector('body')
 
 const scrollBtn = document.querySelector('.scrollBtn');
 
-
 const responsiveBurgerMenu = () => {
-    // Toggle nav bar
+    // Toggle nav bar on small screens
     burgerBtn.addEventListener('click', (e) => {
 
         e.stopPropagation()
@@ -55,11 +54,13 @@ const responsiveBurgerMenu = () => {
     });
 }
 
-// When the user scrolls down 20px from the top of the document, show the button
+responsiveBurgerMenu()
+
 window.onscroll = function () {
     scrollFunction()
 };
 
+//  Show the scroll to top button when the user scrolls down 40px from the top of the document
 function scrollFunction() {
     if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
         scrollBtn.classList.add('fadeIn');
@@ -104,18 +105,78 @@ for (let i = 0; i < AlertCloseButtons.length; i++) {
     });
 }
 
-// Use Ajax to display a success or error alert when the form is submitted
 const contactForm = document.querySelector('.contact-form')
+const nameInput = contactForm.elements['name']
+const emailInput = contactForm.elements['email']
+const messageInput = contactForm.elements['message']
+
+const inputs = [nameInput, emailInput, messageInput]
+
+const resetInput = (input) => {
+    const formControl = input.parentElement;
+    formControl.classList.remove('invalid')
+}
+
+const inputError = (input, message) => {
+    const formControl = input.parentElement;
+    const errorMsg = formControl.querySelector('.input-error-msg')
+    errorMsg.innerText = message;
+    formControl.classList.add('invalid')
+}
+
+const isEmail = (value) => /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(value);
+
+let isFormValid = false;
+let shouldValidate = false;
+
+const validateInputs = () => {
+    if (!shouldValidate) return
+    isFormValid = true
+
+    const nameValue = nameInput.value.trim()
+    const emailValue = emailInput.value.trim()
+    const messageValue = messageInput.value.trim()
+
+    inputs.forEach(input => resetInput(input))
+
+    if (!nameValue) {
+        isFormValid = false;
+        inputError(nameInput, 'Name field is required!')
+    }
+
+    if (!emailValue) {
+        isFormValid = false;
+        inputError(emailInput, 'Email field is required!')
+    } else if (!isEmail(emailValue)) {
+        isFormValid = false;
+        inputError(emailInput, 'A valid email address is required!')
+    }
+
+    if (!messageValue) {
+        isFormValid = false;
+        inputError(messageInput, 'Message field is requires!')
+    }
+}
+
+// Validate inputs and use Ajax to display a success or error alert when the form is submitted
 contactForm.addEventListener("submit", (e) => {
     e.preventDefault()
-    const formData = new FormData(contactForm)
-    fetch('/', {
-        method: 'POST',
-        headers: {"Content-Type": "application/x-www-form-urlencoded"},
-        body: new URLSearchParams(formData).toString()
-    }).then(() => {
-        showAlert(SuccessAlert)
-        //reset form inputs
-        
-    }).catch(() => showAlert(ErrorAlert))
+    shouldValidate = true
+    validateInputs()
+    if (isFormValid) {
+        const formData = new FormData(contactForm)
+        fetch('/', {
+            method: 'POST',
+            headers: {"Content-Type": "application/x-www-form-urlencoded"},
+            body: new URLSearchParams(formData).toString()
+        }).then(() => {
+            showAlert(SuccessAlert)
+            nameInput.value = ''
+            emailInput.value = ''
+            messageInput.value = ''
+        }).catch(() => showAlert(ErrorAlert))
+    }
 })
+
+// Add an event listener for every input field to validate
+inputs.forEach(input => input.addEventListener('input', () => validateInputs()))
